0ea2c0b50fbae4dd07c33810190e3e76244a85e0
Adam Bandurski <adam.bandurski@flex.com>
2025-09-04 12:35:44 +0200

    Dropping unused cases

--- README.md
+++ README.md
@@ -3,10 +3,7 @@
 ### Cases used in tasks
 
 #### Case 1
+
 ![](graph-traversal/case1.png)
-#### Case 2
-![](graph-traversal/case2.png)
-#### Case 3
-![](graph-traversal/case3.png)
 
 
--- graph-traversal/src/main/java/com/flex/interview/graphtraversal/cases/GraphCases.java
+++ graph-traversal/src/main/java/com/flex/interview/graphtraversal/cases/GraphCases.java
@@ -41,23 +41,4 @@ public class GraphCases {
 
 		return new Graph(List.of(guad, shen));
 	}
-
-	public static Graph case3() {
-		Product pcba = new Product("PCBA", 100, 2);
-		Product aluminiumCase = new Product("Aluminium Case", 50, 1);
-		Product cables = new Product("Cables", 0.02, 2);
-
-		Site tczew = new Site("Tczew", "POLAND");
-		Site guad = new Site("Guadalajara", "MEXICO");
-		Site shen = new Site("Shenzhen", "CHINA");
-		Site austin = new Site("Austin", "USA");
-
-		shen.addLane(pcba, tczew);
-		guad.addLane(aluminiumCase, austin, 0.5);
-		guad.addLane(aluminiumCase, tczew, 0.5);
-		austin.addLane(aluminiumCase, tczew);
-		guad.addLane(cables, tczew);
-
-		return new Graph(List.of(guad, shen));
-	}
 }
